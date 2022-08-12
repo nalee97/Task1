21e2fa30cdda7cb70668a3b8e47ae4417772abf0
nalee97 <65673650+nalee97@users.noreply.github.com>
2022-08-12 19:02:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Mailjet.SendmailService
+{
+    public static class Email
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+        public static async Task RunAsync()
+        {
+            MailjetClient client = new MailjetClient(Environment.GetEnvironmentVariable("****************************1234"), Environment.GetEnvironmentVariable("****************************abcd"))
+            {
+                //Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "mohamednaleef1564@gmail.com"},
+        {"Name", "Mohamed"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          "mohamednaleef1564@gmail.com"
+         }, {
+          "Name",
+          "Mohamed"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       "Greetings from Mailjet."
+      }, {
+       "TextPart",
+       "My first Mailjet email"
+      }, {
+       "HTMLPart",
+       "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!"
+      }, {
+       "CustomID",
+       "AppGettingStartedTest"
+      }
+     }
+             });
+            MailjetResponse response = await client.PostAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+                Console.WriteLine(response.GetData());
+            }
+            else
+            {
+                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+                Console.WriteLine(response.GetData());
+                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+            }
+        }
+    }
+}
